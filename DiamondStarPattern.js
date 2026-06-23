df4b743c80e8b333211c9c819b187b6a9992e244
Ashish Saluja <ashishsaluja1@gmail.com>
2026-06-23 17:01:43 +0200

    Diamond star pattern

---
+++ DiamondStarPattern.js
@@ -0,0 +1,24 @@
+const numberOfRows = 8;
+for (let i = 1; i <= numberOfRows; i++) {
+  let row = "";
+  for (let j = 1; j <= numberOfRows - i; j++) {
+    row += " ";
+  }
+
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    row += "*";
+  }
+  console.log(row);
+}
+
+for (let i = numberOfRows - 1; i >= 1; i--) {
+  let row = "";
+  for (let j = 1; j <= numberOfRows - i; j++) {
+    row += " ";
+  }
+
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    row += "*";
+  }
+  console.log(row);
+}
