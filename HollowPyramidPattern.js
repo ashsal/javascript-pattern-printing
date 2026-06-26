942658e293a69564e360a3c9de15a327dafe58f6
Ashish Saluja <ashishsaluja1@gmail.com>
2026-06-26 13:08:41 +0200

    Hollow pyramid pattern

---
+++ HollowPyramidPattern.js
@@ -0,0 +1,16 @@
+const numberOfRows = 5;
+for (let i = 1; i <= numberOfRows; i++) {
+  let row = "";
+  for (let j = 1; j <= numberOfRows - i; j++) {
+    row += " ";
+  }
+
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    if (k === 1 || k === 2 * i - 1 || i === numberOfRows) {
+      row += "*";
+    } else {
+      row += " ";
+    }
+  }
+  console.log(row);
+}
